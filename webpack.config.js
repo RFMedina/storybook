ba5048069158a219ca182c30a7053db69adcbc97
RFMedina <fmedinaraul@gmail.com>
2023-03-11 16:15:05 +0100

    feat: NPM update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
